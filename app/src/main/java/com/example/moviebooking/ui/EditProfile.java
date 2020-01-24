741e9d97161fc888340eaa63463116ec94eb3344
Zeyad3ly <zeyadaly15@gmail.com>
2020-01-25 00:21:43 +0200

    Created Profile Activity Created Edit Profile Activity

---
+++ app/src/main/java/com/example/moviebooking/ui/EditProfile.java
@@ -0,0 +1,16 @@
+package com.example.moviebooking.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.moviebooking.R;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
