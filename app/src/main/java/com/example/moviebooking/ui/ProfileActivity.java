d7c5a21507a06a4e4dad505173733af00381be25
Zeyad3ly <zeyadaly15@gmail.com>
2020-01-25 02:13:15 +0200

    Created Profile Activity Created Edit Profile Activity New Visuals Bugs Fixed Added More Bugs to fix later

--- app/src/main/java/com/example/moviebooking/ui/ProfileActivity.java
+++ app/src/main/java/com/example/moviebooking/ui/ProfileActivity.java
@@ -1,5 +1,16 @@
 package com.example.moviebooking.ui;
 
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,13 +19,48 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.example.moviebooking.R;
+import com.example.moviebooking.data.UserModel;
+import com.example.moviebooking.viewModel.ProfileViewModel;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnBack)
+    ImageView btnBack;
+    @BindView(R.id.btnEditProfile)
+    ImageView btnEditProfile;
+    @BindView(R.id.topbar)
+    LinearLayout topbar;
+    @BindView(R.id.imgProfilePic)
+    ImageView imgProfilePic;
+    @BindView(R.id.cardView)
+    CardView cardView;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    ProfileViewModel profileViewModel;
+    @BindView(R.id.user_name_tv)
+    TextView userNameTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        ButterKnife.bind(this);
+        profileViewModel = ViewModelProviders.of(this).get(ProfileViewModel.class);
+        profileViewModel.getMutableLiveData(this).observe(this, new Observer<UserModel>() {
+            @Override
+            public void onChanged(UserModel userModel) {
+            userNameTv.setText(userModel.getName());
+
+
+            }
+        });
+
+
+
 
         ImageView btnEditProfile = findViewById(R.id.btnEditProfile);
         btnEditProfile.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +80,6 @@ public class ProfileActivity extends AppCompatActivity {
 
             }
         });
+
     }
 }
