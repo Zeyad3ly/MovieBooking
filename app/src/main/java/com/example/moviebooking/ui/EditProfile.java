dd7ff499e69df250d63d223f6710fb4ff0d35e3a
Zeyad3ly <zeyadaly15@gmail.com>
2020-01-25 02:04:59 +0200

    Merge branch 'master' of C:\Users\Drake\AndroidStudioProjects\MovieBooking with conflicts.

--- app/src/main/java/com/example/moviebooking/ui/EditProfile.java
+++ app/src/main/java/com/example/moviebooking/ui/EditProfile.java
@@ -2,7 +2,11 @@ package com.example.moviebooking.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.moviebooking.R;
 
@@ -12,5 +16,7 @@ public class EditProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+
     }
 }
--- app/src/main/java/com/example/moviebooking/ui/ProfileActivity.java
+++ app/src/main/java/com/example/moviebooking/ui/ProfileActivity.java
@@ -1,56 +1,38 @@
 package com.example.moviebooking.ui;
 
-import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.moviebooking.R;
-import com.example.moviebooking.data.UserModel;
-import com.example.moviebooking.viewModel.ProfileViewModel;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class ProfileActivity extends AppCompatActivity {
-
-    @BindView(R.id.btnBack)
-    ImageView btnBack;
-    @BindView(R.id.btnEditProfile)
-    ImageView btnEditProfile;
-    @BindView(R.id.topbar)
-    LinearLayout topbar;
-    @BindView(R.id.imgProfilePic)
-    ImageView imgProfilePic;
-    @BindView(R.id.cardView)
-    CardView cardView;
-    @BindView(R.id.recyclerView)
-    RecyclerView recyclerView;
-    ProfileViewModel profileViewModel;
-    @BindView(R.id.user_name_tv)
-    TextView userNameTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        ButterKnife.bind(this);
-        profileViewModel = ViewModelProviders.of(this).get(ProfileViewModel.class);
-        profileViewModel.getMutableLiveData(this).observe(this, new Observer<UserModel>() {
+
+        ImageView btnEditProfile = findViewById(R.id.btnEditProfile);
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(UserModel userModel) {
-            userNameTv.setText(userModel.getName());
-
-
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), EditProfile.class);
+                startActivity(intent);
             }
         });
 
+        ImageView btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent2);
 
+            }
+        });
     }
 }
